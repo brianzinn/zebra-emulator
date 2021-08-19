0345407a80eabb6ff90688720988a86640b6a4cf
Brian Zinn <github@wakeskate.com>
2021-08-19 10:28:41 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # zebra-emulator
 Paper saver that emulates server needed for BrowserPrint (Zebra ZPL)
+
+Runs a service that acts like the server that runs on your computer, which sends the ZPL to the Zebra Printer from the browser (Browser Print).
+
+It will log what is being sent to the printer.  You can upload to an online viewer - that would be a good enhancement is for this program to display what would be printed (like ngrok request viewer) :)
---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ type: '*/*' }));
+
+app.get('/default', (req, res) => {
+  res.json({
+    "deviceType": "printer",
+    "uid": "Zebra ZP 500 (ZPL)",
+    "provider": "com.zebra.ds.webdriver.desktop.provider.DefaultDeviceProvider",
+    "name": "Zebra ZP 500 (ZPL)",
+    "connection": "driver",
+    "version": 3,
+    "manufacturer": "Zebra Technologies"
+  })
+})
+
+app.get('/available', (req, res) => {
+  res.json({
+    "deviceList": [
+      {
+        "uid": 1,
+        "name": "Fake Printer"
+      }
+    ]
+  })
+})
+
+app.post('/write', (req, res) => {
+  console.log('data:', req?.body?.data);
+  console.log('---------------------------------------\n')
+  res.json({})
+})
+
+const SERVER_PORT = 9100;
+app.listen(SERVER_PORT, () =>
+  console.log(`Browser Print Fake Server running on http://localhost:${SERVER_PORT}`)
+);
