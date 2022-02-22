8e7923cda6dd7fd64b79990db88fc29626723102
mbsmooth <mbsmooth@gmail.com>
2022-02-22 12:34:04 -0600

    Add SSL certs & service; add POST /read route

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
-
+import https from 'https'
+import fs from 'fs'
 import * as dotenv from 'dotenv';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json({ type: '*/*' }));
 
 app.get('/default', (req, res) => {
+  console.log('GET request for /default recieved')
   res.json({
     "deviceType": "printer",
     "uid": "Zebra ZP 500 (ZPL)",
@@ -22,21 +24,28 @@ app.get('/default', (req, res) => {
 })
 
 app.get('/available', (req, res) => {
+  console.log('GET request for /available recieved')
   res.json({
     "deviceList": [
       {
-        "uid": 1,
-        "name": "Fake Printer"
+        "uid": "Zebra ZP 500 (ZPL)",
+        "name": "Zebra ZP 500 (ZPL)"
       }
     ]
   })
 })
 
+app.post('/read', (req, res) => {
+  console.log('POST request for /read recieved')
+  res.status(200).send();
+})
+
 app.post('/write', (req, res) => {
+  console.log('POST request for /write recieved')
   console.log('data:', (req && req.body) ? req.body.data : '"data" missing req.body');
   console.log('---------------------------------------\n')
   if (process.env.CHROME_EXTENSION_ENABLED === String(true)) {
-    const port = process.env.CHROME_EXTENSION_PORT ?? '9101';
+    const port = process.env.CHROME_EXTENSION_PORT ?? '9102';
     // const payload = typeof req.body.data === 'string' ? req.body.data : Buffer.from(req.body.data);
     fetch(`http://localhost:${port}`, { method: 'POST', body: '{"mode":"print","epl":"' + Buffer.from(req.body.data) + '"}' })
       .catch((e) => {
@@ -47,8 +56,31 @@ app.post('/write', (req, res) => {
 })
 
 const SERVER_PORT = 9100;
+const SERVER_PORT_SSL = 9101;
 const EXTENSION_PORT = process.env.CHROME_EXTENSION_PORT ?? '9101';
-app.listen(SERVER_PORT, () => {
+
+//SSL key and cert
+var option = {
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.cert')
+};
+
+
+https.createServer(option, app).listen(SERVER_PORT_SSL, () => {
+  console.log(`\nBrowser Print Fake Server running on https://localhost:${SERVER_PORT}\n\n`)
+  console.log('******************************************************************************')
+  if (process.env.CHROME_EXTENSION_ENABLED === String(true)) {
+    console.log(
+      '  TIP: If you want to preview the label you can install the Zpl Printer\n  extension from the chrome web store and set it up to listen on port ' + EXTENSION_PORT
+    )
+  } else { 
+    console.log('Chrome extension support not enabled.');
+  }
+  console.log('******************************************************************************\n')
+});
+
+
+app.listen(SERVER_PORT,'127.0.0.1', () => {
   console.log(`\nBrowser Print Fake Server running on http://localhost:${SERVER_PORT}\n\n`)
   console.log('******************************************************************************')
   if (process.env.CHROME_EXTENSION_ENABLED === String(true)) {
